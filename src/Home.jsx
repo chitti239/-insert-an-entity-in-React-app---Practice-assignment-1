feaad96bb7fed524da62b123530a887b7a19a3c2
chitti239 <sharinis239@gmail.com>
2025-04-22 14:10:20 +0530

    Completed the assignment

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,8 +4,10 @@ import React from "react";
 import BookCard from "./components/BookCard";
 import books from "./booksData";
 import "./Home.css"; // Import the CSS for styling
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div className="home-container">
       <h1>Book Library</h1>
@@ -14,8 +16,12 @@ const Home = () => {
           <BookCard key={book.id} book={book} />
         ))}
       </div>
+      <div>
+        <button onClick={()=>navigate("/add-book")}>Add book</button>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
